4fdb246c64bfccb9104eef3fb48e60bd01ee3f7c
Alain Gilbert <alain.gilbert.15@gmail.com>
2017-02-25 14:38:14 -0800

    fix webpack errs

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -12,17 +12,17 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
   ],
   module: {
     loaders: [
-      { test: /\.jsx?$/, loaders: ['babel'], include: path.join(__dirname, 'src') },
-      { test: /\.scss$/, loader: 'style!css!sass' },
-      { test: /\.json$/, loader: 'json' },
+      { test: /\.jsx?$/, loaders: ['babel-loader'], include: path.join(__dirname, 'src') },
+      { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' },
+      { test: /\.json$/, loader: 'json-loader' },
       { test: /\.woff2?($|\?)/, loader: 'url?limit=10000&mimetype=application/font-woff' },
     ],
   },
   resolve: {
-    extensions: ['', '.js'],
+    extensions: ['*', '.js'],
   },
 };
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -11,7 +11,7 @@ module.exports = {
     publicPath: '/public/',
   },
   plugins: [
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false,
@@ -36,20 +36,19 @@ module.exports = {
       },
       sourceMap: false,
     }),
-    new webpack.optimize.DedupePlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
   ],
   module: {
     loaders: [
-      { test: /\.jsx?$/, loaders: ['babel'], include: path.join(__dirname, 'src') },
-      { test: /\.scss$/, loader: 'style!css!sass' },
-      { test: /\.json$/, loader: 'json' },
+      { test: /\.jsx?$/, loaders: ['babel-loader'], include: path.join(__dirname, 'src') },
+      { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' },
+      { test: /\.json$/, loader: 'json-loader' },
       { test: /\.woff2?($|\?)/, loader: 'url?limit=10000&mimetype=application/font-woff' },
     ],
   },
   resolve: {
-    extensions: ['', '.js'],
+    extensions: ['*', '.js'],
   },
 };
