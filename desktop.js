e64dda76ee2c1e212600203457e9e2a7c30ec87d
Alain Gilbert <alain.gilbert.15@gmail.com>
2017-02-25 17:23:37 -0800

    Fix copy/paste + Dock icon

--- desktop.js
+++ desktop.js
@@ -1,5 +1,6 @@
 const menubar = require('menubar');
 const path = require('path');
+const { Menu, app } = require('electron');
 
 // enable ES6 (transpiling behind the scene)
 require('babel-register');
@@ -7,9 +8,32 @@ require('babel-register');
 // start server
 require('./src/server.js');
 
+app.on('ready', () => {
+  // Create the Application's main menu
+  var template = [{
+    label: "Application",
+    submenu: [
+      { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+      { type: "separator" },
+      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { type: "separator" },
+      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
+  ];
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+});
+
 menubar({
   preloadWindow: true,
-  showDockIcon: true,
+  showDockIcon: false,
   icon: path.join(__dirname, '/public/icons/UberLogoTemplate.png'),
   transparent: true,
   movable: false,
